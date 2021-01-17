499a03ed0b6c4d48168c888aa2b0eeb0945f1a40
mikail aktas <mikail.aktas1@hotmail.com>
2021-01-17 13:35:41 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSaisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if(dolarDun<dolarBugun)
+
+            {
+                Console.WriteLine("Artış butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("degişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap butonu" );
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
