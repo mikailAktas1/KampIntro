9b9385d999fe52cc2bd450b6e711396335cdbac7
mikail aktas <mikail.aktas1@hotmail.com>
2021-01-23 23:54:11 +0300

    23.01.2021 dersleri

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        Musteri musteri = new Musteri();
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi." + " " + musteri.Adi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi.");
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi );
+            }
+        }
+    }
+}
